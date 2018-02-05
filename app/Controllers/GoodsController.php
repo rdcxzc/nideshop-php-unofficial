3da03ca14d84b6094a5d93e5378fd88ee0dfe3f6
muchang <zhaocheng@zhaojiafang.com>
2018-02-05 18:02:59 +0800

    xiugai

--- app/Controllers/GoodsController.php
+++ app/Controllers/GoodsController.php
@@ -9,12 +9,17 @@
 namespace App\Controllers;
 
 
+use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Goods;
+use App\Models\GoodsAttribute;
 use App\Models\SearchHistory;
+use App\Models\User;
 use Slim\Exception\Exception;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use think\Db;
 
 class GoodsController extends Controller
 {
@@ -150,9 +155,81 @@ class GoodsController extends Controller
 
     public function detail(Request $request, Response $response)
     {
-        $id = $request->getParam('id');
-        $str = '{"errno":0,"errmsg":"","data":{"info":{"id":1166008,"category_id":1005007,"goods_sn":"1166008","name":"Carat钻石 不粘厨具组合","brand_id":0,"goods_number":100,"keywords":"","goods_brief":"钻石涂层，不粘锅锅具组","goods_desc":"","is_on_sale":1,"add_time":0,"sort_order":5,"is_delete":0,"attribute_category":0,"counter_price":0,"extra_price":0,"is_new":1,"goods_unit":"只","primary_pic_url":"http://yanxuan.nosdn.127.net/056baf67bb8cc9a4f2544ac5954ab67c.jpg","list_pic_url":"http://yanxuan.nosdn.127.net/615a16e899e01efb780c488df4233f48.png","retail_price":459,"sell_volume":889,"primary_product_id":1178050,"unit_price":0,"promotion_desc":"限时购","promotion_tag":"","app_exclusive_price":0,"is_app_exclusive":0,"is_limited":0,"is_hot":0},"gallery":[],"attribute":[],"userHasCollect":0,"issue":[{"id":1,"goods_id":"1127052","question":"购买运费如何收取？","answer":"单笔订单金额（不含运费）满88元免邮费；不满88元，每单收取10元运费。\n(港澳台地区需满"},{"id":2,"goods_id":"1127052","question":"使用什么快递发货？","answer":"严选默认使用顺丰快递发货（个别商品使用其他快递），配送范围覆盖全国大部分地区（港澳台地区除"},{"id":3,"goods_id":"1127052","question":"如何申请退货？","answer":"1.自收到商品之日起30日内，顾客可申请无忧退货，退款将原路返还，不同的银行处理时间不同，"},{"id":4,"goods_id":"1127052","question":"如何开具发票？","answer":"1.如需开具普通发票，请在下单时选择“我要开发票”并填写相关信息（APP仅限2.4.0及以"}],"comment":{"count":0,"data":{}},"brand":{},"specificationList":[],"productList":[{"id":244,"goods_id":1166008,"goods_specification_ids":"","goods_sn":"1166008","goods_number":100,"retail_price":459}]}}';
-        return $this->api($str, $response);
+        $goodsId = $request->getParam('id');
+        $goodsModel = new Goods();
+        $goodsAttributeModel = new GoodsAttribute();
+        $brandModel = new Brand();
+        $commentModel = new Comment();
+        $userModel = new User();
+        if(empty($goodsId)){
+            throw new \think\Exception('参数不正确');
+        }
+
+
+        $info = $goodsModel->where(['id' => $goodsId])->find();
+        $gallery = Db::name('goods_gallery')->where(['goods_id' => $goodsId])->limit(4)->select();
+        $attribute = $goodsAttributeModel->getGoodsAttribute($goodsId);
+        $issue = Db::name('goods_issue')->select();
+        $brand = $brandModel->where(['id' => $info['brand_id']])->select()->toArray();
+        $commentCount = $commentModel->where(['value_id' => $goodsId ,'type_id' => 0])->count('id');
+        $hotComment = $commentModel->where(['value_id' => $goodsId ,'type_id' => 0])->find()->toArray();
+
+        $commentInfo = [];
+        if(!empty($hotComment)){
+            $commentUser = $userModel->field('nickname,username,avatar')->where(['id' => $hotComment['user_id']])->find()->toArray();
+            $picList = Db::name('comment_picture')->where(['comment_id' => $hotComment['id']])->select();
+            $commentInfo = [
+                'content'  => base64_decode($hotComment['content']),
+                'add_time' => time(),
+                'nickname' => $commentUser['nickname'],
+                'avatar'   => $commentUser['avatar'],
+                'pic_list' => $picList
+            ];
+        }
+        /**
+         *
+        const issue = await this.model('goods_issue').select();
+        const brand = await this.model('brand').where({id: info.brand_id}).find();
+        const commentCount = await this.model('comment').where({value_id: goodsId, type_id: 0}).count();
+        const hotComment = await this.model('comment').where({value_id: goodsId, type_id: 0}).find();
+        let commentInfo = {};
+        if (!think.isEmpty(hotComment)) {
+        const commentUser = await this.model('user').field(['nickname', 'username', 'avatar']).where({id: hotComment.user_id}).find();
+        commentInfo = {
+        content: new Buffer(hotComment.content, 'base64').toString(),
+        add_time: think.datetime(new Date(hotComment.add_time * 1000)),
+        nickname: commentUser.nickname,
+        avatar: commentUser.avatar,
+        pic_list: await this.model('comment_picture').where({comment_id: hotComment.id}).select()
+        };
+        }
+
+        const comment = {
+        count: commentCount,
+        data: commentInfo
+        };
+
+        // 当前用户是否收藏
+        const userHasCollect = await this.model('collect').isUserHasCollect(think.userId, 0, goodsId);
+
+        // 记录用户的足迹 TODO
+        await await this.model('footprint').addFootprint(think.userId, goodsId);
+
+        // return this.json(jsonData);
+        return this.success({
+        info: info,
+        gallery: gallery,
+        attribute: attribute,
+        userHasCollect: userHasCollect,
+        issue: issue,
+        comment: comment,
+        brand: brand,
+        specificationList: await model.getSpecificationList(goodsId),
+        productList: await model.getProductList(goodsId)
+        });
+         *
+         */
+//        return $this->api($str, $response);
     }
 
     public function index(Request $request, Response $response)
