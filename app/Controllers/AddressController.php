134f332ce284f128549b238ac236bf63442e1202
muchang <zhaocheng@zhaojiafang.com>
2018-02-24 13:44:50 +0800

    xiugai

--- app/Controllers/AddressController.php
+++ app/Controllers/AddressController.php
@@ -17,18 +17,18 @@ class AddressController extends Controller
     public function getList()
     {
 
-        $addressList = Db::name('address')->where()->select()->toArray();
-        const addressList = yield _this.model('address').where({ user_id: think.userId }).select();
-      let itemKey = 0;
-      for (const addressItem of addressList) {
-        addressList[itemKey].province_name = yield _this.model('region').getRegionName(addressItem.province_id);
-        addressList[itemKey].city_name = yield _this.model('region').getRegionName(addressItem.city_id);
-        addressList[itemKey].district_name = yield _this.model('region').getRegionName(addressItem.district_id);
-        addressList[itemKey].full_region = addressList[itemKey].province_name + addressList[itemKey].city_name + addressList[itemKey].district_name;
-        itemKey += 1;
-    }
+//        $addressList = Db::name('address')->where()->select()->toArray();
+//        const addressList = yield _this.model('address').where({ user_id: think.userId }).select();
+//      let itemKey = 0;
+//      for (const addressItem of addressList) {
+//        addressList[itemKey].province_name = yield _this.model('region').getRegionName(addressItem.province_id);
+//        addressList[itemKey].city_name = yield _this.model('region').getRegionName(addressItem.city_id);
+//        addressList[itemKey].district_name = yield _this.model('region').getRegionName(addressItem.district_id);
+//        addressList[itemKey].full_region = addressList[itemKey].province_name + addressList[itemKey].city_name + addressList[itemKey].district_name;
+//        itemKey += 1;
+//    }
 
-      return _this.success(addressList);
+//      return _this.success(addressList);
     }
 
 }
